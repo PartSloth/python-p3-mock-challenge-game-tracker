9adb592cb693f4505699ba192dbfb2f108fe9308
Louiza Mak <louiza.mak@gmail.com>
2024-05-08 14:17:15 -0500

    complete lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,114 @@
 class Game:
     def __init__(self, title):
         self.title = title
+        self._results = []
+        self._players = []
 
-    def results(self):
-        pass
+    @property
+    def title(self):
+        return self._title
 
-    def players(self):
-        pass
+    @title.setter
+    def title(self, title):
+        if hasattr(self, 'title'):
+            raise Exception
+        else:
+            if isinstance(title, str) and len(title) > 0:
+                self._title = title
+            else:
+                raise Exception
+
+    def results(self, result=None):
+        if isinstance(result, Result):
+            self._results.append(result)
+        return self._results
+        
+    def players(self, player=None):
+        if isinstance(player, Player) and player not in self._players:
+            self._players.append(player)
+        return self._players
 
     def average_score(self, player):
-        pass
+        player_results = []
+        for result in self._results:
+            if result.player == player:
+                player_results.append(result.score)
+            
+        return sum(player_results) / len(player_results)
 
 class Player:
     def __init__(self, username):
         self.username = username
+        self._results = []
+        self._games = []
+    
+    @property
+    def username(self):
+        return self._username
 
-    def results(self):
-        pass
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 2 <= len(username) <= 16:
+            self._username = username
+        else: 
+            raise Exception
 
-    def games_played(self):
-        pass
+    def results(self, result=None):
+        if isinstance(result, Result):
+            self._results.append(result)
+        return self._results
+
+    def games_played(self, game=None):
+        if isinstance(game, Game) and game not in self._games:
+            self._games.append(game)
+        return self._games
 
     def played_game(self, game):
-        pass
+        return game in self._games
 
     def num_times_played(self, game):
-        pass
+        return sum(1 for result in self._results if result.game == game)
 
 class Result:
+
+    all = []
+
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
+        game.results(self)
+        game.players(player)
+        player.results(self)
+        player.games_played(game)
+        Result.all.append(self)
+        
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self, score):
+        if hasattr(self, 'score'):
+            raise Exception
+        else: 
+            if isinstance(score, int) and 1 <= score <= 5000:
+                self._score = score 
+
+    @property
+    def player(self):
+        return self._player
+
+    @player.setter
+    def player(self, player):
+        if isinstance(player, Player):
+            self._player = player
+
+    @property
+    def game(self):
+        return self._game
+
+    @game.setter
+    def game(self, game):
+        if isinstance(game, Game):
+            self._game = Game
--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,13 @@ from classes.many_to_many import Result
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+    player = Player("Sleepy")
+    player_2 = Player("Toad")
+    game = Game("Valorant")
+    Result(player, game, 3000)
+    Result(player, game, 1500)
+    Result(player_2, game, 2000)
+
+    player.num_times_played(game)
+
     ipdb.set_trace()
